79769fe1f706f1e1f91394f3036886bc6727d20f
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2023-02-16 19:10:00 +0100

    Create Users.sql

---
+++ Users.sql
@@ -0,0 +1,4 @@
+SOQL Queries on User In Salesforce:
+===================================
+- Find Count of Users For Each Profile in Salesforce
+SELECT count(Id), Profile.Name FROM User WHERE User.IsActive = True GROUP BY Profile.Name
