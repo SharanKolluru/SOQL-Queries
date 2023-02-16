7b10cc861d189260d1061b29e55b4d727c35d0c1
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2023-02-16 23:01:10 +0100

    Update Users.sql

--- Users.sql
+++ Users.sql
@@ -3,5 +3,8 @@ SOQL Queries on User In Salesforce:
 - Find Count of Users For Each Profile in Salesforce
 SELECT count(Id), Profile.Name FROM User WHERE User.IsActive = True GROUP BY Profile.Name
 
+- Find List of User For Each Profile using IN Operator
+SELECT Id, Name, Profile.Name, IsActive From User WHERE Profile.Name IN ('System Administrator','System Administrator Plus') AND IsActive = True
+
 
 
