cf9b9f37749a891bd524a7f4d5d970a37b91f2e1
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2023-05-18 21:34:24 +0200

    Create List - All Available Formulas For Salesforce Flow.sql

---
+++ List - All Available Formulas For Salesforce Flow.sql
@@ -0,0 +1,5 @@
+Step 01: Click Setup | Developer Console.
+Step 02: In the Query Editor, enter the following SOQL query 
+SELECT Function.Name, Function.Label, Function.Description, Function.ExampleString FROM FormulaFunctionAllowedType where Type=’Flow’
+Select the Use Tooling API checkbox.
+Step 03: Click Execute.
