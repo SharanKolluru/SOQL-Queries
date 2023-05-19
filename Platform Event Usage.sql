cf026790309529bf27a9110ac410ba0b952dbf9b
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2023-05-19 15:04:18 +0200

    Update Platform Event Usage.sql

--- Platform Event Usage.sql
+++ Platform Event Usage.sql
@@ -1,11 +1,11 @@
-👉 **Event Usage Metrics for the Last 24 Hours:-**
+👉 Event Usage Metrics for the Last 24 Hours:-
 
 SELECT Name, StartDate, EndDate, Value FROM PlatformEventUsageMetric WHERE StartDate = LAST_N_DAYS:1
 
-👉 **Platform Event Subscribers:-**
+👉 Platform Event Subscribers:-
 
 SELECT ExternalId, Name, Position, Status, Tip FROM EventBusSubscriber
 
-👉 **Platform Event Channel Members:-**
+👉 Platform Event Channel Members:-
 
 SELECT Id, DeveloperName, EventChannel, SelectedEntity FROM PlatformEventChannelMember
