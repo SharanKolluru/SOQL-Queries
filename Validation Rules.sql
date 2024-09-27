25cb2d586def4d5eda77b5b541bba83d4ec0a794
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2024-09-27 19:16:05 +0200

    Update Query & File Rename

--- Validation Rules.sql
+++ Validation Rules.sql
@@ -1,2 +1,4 @@
+👉  Query To Get ALL Validation Rules in Salesforce :-
+
 SELECT Id, ValidationName, Active, Description, EntityDefinition.DeveloperName, ErrorDisplayField, ErrorMessage FROM Validationrule
 - Use Tooling API
