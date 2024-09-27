302d4c49ebeeb1e109c944bd8e6b9ddce5c906c9
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2024-09-27 19:12:51 +0200

    Update Managed Packages.sql

--- Managed Packages.sql
+++ Managed Packages.sql
@@ -1,3 +1,13 @@
+👉 Information about Installed Packages in Salesforce via SOQL Query:-
+List<PackageLicense> installedPackages = [SELECT Id, NamespacePrefix, AllowedLicenses, UsedLicenses, ExpirationDate, Status FROM PackageLicense];
+for (PackageLicense app : installedPackages) {
+    System.debug('App Namespace: ' + app.NamespacePrefix);
+	  System.debug('Allowed Licenses: ' + app.AllowedLicenses);
+    System.debug('Used Licenses: ' + app.UsedLicenses);
+  	System.debug('Expiration Date: ' + app.ExpirationDate);
+  	System.debug('Status: ' + app.Status);
+}
+
 👉 Installed Managed Packages & Its Versions:-
 
 SELECT Id, SubscriberPackageId, SubscriberPackage.NamespacePrefix, SubscriberPackage.Name, SubscriberPackageVersion.Id, SubscriberPackageVersion.Name, SubscriberPackageVersion.MajorVersion, SubscriberPackageVersion.MinorVersion, SubscriberPackageVersion.PatchVersion, SubscriberPackageVersion.BuildNumber FROM InstalledSubscriberPackage
