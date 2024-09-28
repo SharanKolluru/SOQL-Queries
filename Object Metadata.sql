ea2ef8a31f22bf7b2bf90d8f29bd210417d95af0
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2024-09-28 14:20:02 +0200

    Update Object Metadata

--- Object Metadata.sql
+++ Object Metadata.sql
@@ -1,5 +1,4 @@
-SOQL Queries on Object In Salesforce To Get all Field Definition (Standard & Custom Fields):
-===================================
+👉 SOQL Queries on Object In Salesforce To Get all Field Definition (Standard & Custom Fields):
   
 SELECT QualifiedApiName, Label, DeveloperName, Description, DataType, ExtraTypeInfo, Length, Precision, Scale 
 FROM FieldDefinition 
