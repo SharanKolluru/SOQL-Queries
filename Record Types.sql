dc5be48d6dcbf22d9186713dd2886ab0d23e87ac
Sharan Kumar  Kolluru <88401843+SharanKolluru@users.noreply.github.com>
2024-09-28 14:30:38 +0200

    Update Record Types

--- Record Types.sql
+++ Record Types.sql
@@ -1,12 +1,12 @@
-**Retrieving sObject RecordTypeId in Apex**
+Retrieving sObject RecordTypeId in Apex
+========================================
 
-**Method #1:** SOQL Query To Fetch List Of Record Types For an sObject (Example: Lead, Account, Contact, Opportunity....)
-- SELECT Id, Name FROM RecordType WHERE sObjectType = 'lead'
-- SELECT Id, Name FROM RecordType WHERE sObjectType = 'lead' AND DeveloperName = 'McM_Lead' LIMIT 1
+✍️ Method 1: SOQL Query To Fetch List Of Record Types For an sObject (Example: Lead, Account, Contact, Opportunity....)
+- SELECT Id, Name FROM RecordType WHERE sObjectType = 'Lead'
+- SELECT Id, Name FROM RecordType WHERE sObjectType = 'Lead' AND DeveloperName = 'Customer' LIMIT 1
 
-![image](https://user-images.githubusercontent.com/88401843/128869488-70532290-cd39-40b3-9563-b734ab57b866.png)
-
-**Method #2:** Instead of SOQL query, Its preferable to use Dynamic APEX to get RecordTypeId.
+✍️ Method 2: Instead of SOQL Query, Its preferable to use Dynamic APEX to get RecordTypeId.
+  
 - ID RecordTypeId = Schema.SObjectType.Account.getRecordTypeInfosByName().get('Person Account').getRecordTypeId();
 - ID RecordTypeId = Schema.getGlobalDescribe().get('Account').getDescribe().getRecordTypeInfosByName().get('Person Account').getRecordTypeId();
 
